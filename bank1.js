54787f829bb07a1c00572893cc041fa1999e397d
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-11-21 01:45:48 +0800

    Update bank1.js

--- bank1.js
+++ bank1.js
@@ -2,7 +2,7 @@ bank1 = [
 ['logo','facebook%20messenger',2,'Facebook Messenger'],
 ['logo','facebook',2,'Facebook'],
 ['logo','instagram',3,'Instagram'],
-['logo','snapchat',2,'Snapchat'],
+['logo','snapchat',1,'Snapchat'],
 ['logo','telegram',2,'Telegram'],
 ['logo','tiktok',1,'Tiktok'],
 ['logo','twitter',2,'Twitter'],
