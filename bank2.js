c37a1f8d057713008c49d8f3ce520a3216020923
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-11-21 03:08:13 +0800

    Add files via upload

---
+++ bank2.js
@@ -0,0 +1,10 @@
+onePieceCharLists = ['Shirohige','Ace','Marco','Lucci'];
+
+bank2 = [
+['Kuisku','What is the nickname that Pekalongan City is known for?','Batik City',['Kebaya City','Angklung City','Prehistorical City','Erepsin City']],
+['Kuisku','Yami Yami no Mi is a fruit eaten by the Blackbeard pirates\' captain. What is the name of the pirate captain?','Teach',['@["One Piece Character"]']],
+['Kuisku','Images on TV and computer screens are made up of small squares. What are the names of these small squares?','pixels',['polygons','inchs','lines','privateers']],
+['Kuisku','Which of the following ideas is not based on Darwinian thought?','The increase in the number of organisms is faster than the food supply',['Each population tends to multiply because it is able to reproduce','To breed requires enough food and space','Variation is an inherited trait','Animals within a species compete with each other for food, shelter, and ultimately the ability to reproduce']],
+['Kuisku','In Kurdish wedding tradition, the \'henna party\' for the bride is held how many days before the wedding?','1 day',['2 days','4 days','7 days','3 days','@["number","day",1,7]']],
+['Kuisku','One of the form of the needs of human life according to Maslow is','Need for self-actualization',['Court','Trial','School','Economic needs']],
+];
