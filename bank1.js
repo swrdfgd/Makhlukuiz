c2cda6f676326d697f0a9c16ca63575b3e12e004
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-11-21 00:56:19 +0800

    Add files via upload

---
+++ bank1.js
@@ -0,0 +1,12 @@
+bank1 = [
+['logo','facebook%20messenger',2,'Facebook Messenger'],
+['logo','facebook',2,'Facebook'],
+['logo','instagram',3,'Instagram'],
+['logo','snapchat',2,'Snapchat'],
+['logo','telegram',2,'Telegram'],
+['logo','tiktok',1,'Tiktok'],
+['logo','twitter',2,'Twitter'],
+['logo','vimeo',2,'Vimeo'],
+['logo','whatsapp',3,'WhatsApp'],
+['logo','youtube',2,'Youtube'],
+];
