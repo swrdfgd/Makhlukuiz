6522ff464d667c1509857354f25f18989c3ff0af
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-12-07 08:15:31 +0800

    Add files via upload

---
+++ bank6.js
@@ -0,0 +1,8 @@
+bank6 = [
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/c/c7/Peterbald_male_Shango_by_Irina_Polunina.jpg','Peterbald'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg','Siamese cat'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/1/1a/Serengetimalecat.jpg','Serengeti cat'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/9/92/Ukrainian_Levkoy_cat.jpg','Ukrainian Levkoy'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/5/5d/Adult_Scottish_Fold.jpg','Scottish Fold'],
+];
+ket6 = 'https://randomlistgenerator.com/cat-breeds'
