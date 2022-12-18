220f6b18ce488360f88f4ba70cb5821086d1a0fe
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-12-19 01:51:04 +0800

    Add files via upload

---
+++ bank8.js
@@ -0,0 +1,10 @@
+bankSoal12 = [
+['taksonomi','family','Erythracaridae','order','Trombidiformes'],
+['taksonomi','species','Pothyne rugifrons','family','Cerambycidae'],
+['taksonomi','species','Mordella octoguttata','family','Mordellidae'],
+['taksonomi','species','Wurmbea graniticola','family','Colchicaceae'],
+['taksonomi','species','Lissodrillia verrillii','family','Drilliidae'],
+['taksonomi','species','Streptomyces catenulae','family','Streptomycetaceae'],
+['taksonomi','species','Crocanthes crypsichola','family','Lecithoceridae'],
+['taksonomi','species','Hypolycaena condamini','family','Lycaenidae'],
+]
