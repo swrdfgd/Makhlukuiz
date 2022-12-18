c8f721eda1b230eb2fdd4a55ab9ed0b614a049f6
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2022-12-19 01:39:44 +0800

    Add files via upload

---
+++ bank7.js
@@ -0,0 +1,11 @@
+bank7 = [
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/wild-rice.webp','Wild Rice'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/cauliflower.webp','Cauliflower'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/tempeh.webp','Tempeh'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/okra.webp','Okra'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/sausage.webp','Sausage'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/braised-lamb-cheeks.webp','Braised Lamb Cheeks'],
+['Makanan','https://randomoutputs.com/assets/images/tools/foods/almonds.webp','Almonds'],
+];
+
+ket7 = 'https://randomoutputs.com/random-food-generator';
