21c94f046c23cbf323ce8b296963500d3e20f25f
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2023-04-14 16:36:20 +0800

    Add files via upload

--- bank6.js
+++ bank6.js
@@ -5,5 +5,11 @@ bank6 = [
 ['Kucing','https://upload.wikimedia.org/wikipedia/commons/9/92/Ukrainian_Levkoy_cat.jpg','Ukrainian Levkoy'],
 ['Kucing','https://upload.wikimedia.org/wikipedia/commons/5/5d/Adult_Scottish_Fold.jpg','Scottish Fold'],
 ['Kucing','https://upload.wikimedia.org/wikipedia/commons/6/6a/Oriental_shorthair_20070130_caroline.jpg','Oriental Bicolor'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/5/5a/Maine_Coon_cat_by_Tomitheos.JPG','Maine Coon'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/0/00/Thor%2C_turkish_van.jpg','Turkish Van'],
+['Kucing','https://pixahive.com/wp-content/uploads/2020/09/Arabian-Mau-on-Focus-63609-pixahive.jpg','Arabian Mau'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/3/3a/Russian_blue.jpg','Russian Blue'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/3/31/British_Longhair_cat_with_blue_and_white_markings_and_amber_eyes.jpg','British Longhair'],
+['Kucing','https://upload.wikimedia.org/wikipedia/commons/c/c7/German_rex_harry_%28cropped%29.jpg','German Rex'],
 ];
 ket6 = 'https://randomlistgenerator.com/cat-breeds'
